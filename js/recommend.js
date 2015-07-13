6e7a78ef14868db37e00e28196dee6a5eca33b61
andycall <dongtiangche@gmail.com>
2015-07-13 22:10:40 +0800

    add pic image

--- js/recommend.js
+++ js/recommend.js
@@ -4,9 +4,30 @@ $(document).ready(function(){
 		$(this).parents('.dialog').hide();
 	});
 
-	$('.item').on('click', function(){
 
-	});
+    var imageWidth = $('.bg_image').width();
+    var imageHeight = $('.bg_image').height();
+    //var animateLock = false;
 
+	$(".bg_image").hover(function(e){
+        console.log(e.target);
+        if(e.target.className !== 'bg_image') return;
+        var item = e.target;
+        $(item).animate({
+            width : imageWidth * 1.02,
+            height : imageHeight * 1.02,
+            left : 0 - imageHeight * 0.02 ,
+            top : 0 - imageWidth * 0.02 / 2
+        }, 200);
+    }, function(e){
+        if(e.target.className !== 'bg_image') return;
+        var item = e.target;
+        $(item).animate({
+            width : imageWidth,
+            height : imageHeight,
+            top: 0,
+            left: 0
+        }, 200);
+    });
 
 });
